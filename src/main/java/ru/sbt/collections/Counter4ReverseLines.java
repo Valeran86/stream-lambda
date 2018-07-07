6eae1657f89e12e5d600314e710e05ff7fbe3e30
BukaPuma <melvia@mail.ru>
2018-07-07 19:40:08 +0500

    Домашнее задание по stream api и  лямбда-выражениям

--- src/main/java/ru/sbt/collections/Counter4ReverseLines.java
+++ src/main/java/ru/sbt/collections/Counter4ReverseLines.java
@@ -3,19 +3,59 @@ package ru.sbt.collections;
 import ru.sbt.collections.utils.FileLoader;
 import ru.sbt.collections.utils.StringSplitter;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 /**
  * Выведите на экран все строки файла в обратном порядке.
  */
 public class Counter4ReverseLines {
     public static void main( String[] args ) throws IOException, URISyntaxException {
+        oldCounter();
+        newCounter();
+    }
+
+    public static void oldCounter() throws IOException {
+        long t1 = System.nanoTime();
         String file = FileLoader.loadFile();
         String[] lineArray = StringSplitter.getLines( file );
 
         for ( int i = lineArray.length - 1; i >= 0; i-- ) {
             System.out.println( lineArray[ i ] );
         }
+        long nanoDuration = System.nanoTime() - t1;
+        Duration d = Duration.ofNanos( nanoDuration );
+        System.out.println( d );
+
     }
+
+    public static void newCounter() throws IOException {
+        long t1 = System.nanoTime();
+        String regex= "[;\" + System.lineSeparator() + \" .,]";
+
+        File file = new File("src/main/resources/ru/sbt/collections/VeryBigText.txt");
+
+        Files.lines( Paths.get( file.getAbsolutePath() ) )
+                .flatMap( s -> Stream.of( s.split( regex) ))
+                .collect(Collectors.toCollection(LinkedList::new))
+                .descendingIterator()
+                .forEachRemaining(System.out::println);
+        long nanoDuration = System.nanoTime() - t1;
+        Duration d = Duration.ofNanos( nanoDuration );
+        System.out.println( d );
+    }
+
+
+
 }
--- src/main/java/ru/sbt/collections/Counter5ReverseIterator.java
+++ src/main/java/ru/sbt/collections/Counter5ReverseIterator.java
@@ -3,11 +3,17 @@ package ru.sbt.collections;
 import ru.sbt.collections.utils.FileLoader;
 import ru.sbt.collections.utils.StringSplitter;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Stream;
 
 /**
  * Реализуйте свой Iterator для обхода списка слов в обратном порядке.
@@ -15,6 +21,14 @@ import java.util.List;
 public class Counter5ReverseIterator {
 
     public static void main( String[] args ) throws IOException, URISyntaxException {
+        oldCounter();
+        newCounter();
+
+
+    }
+
+    public static void oldCounter() throws IOException {
+        long t1 = System.nanoTime();
         String file = FileLoader.loadFile();
         List<String> words = Arrays.asList( StringSplitter.getWords( file ) );
 
@@ -22,6 +36,22 @@ public class Counter5ReverseIterator {
 
         while ( reverseArrayIterator.hasNext() )
             System.out.println( reverseArrayIterator.next() );
+        long nanoDuration = System.nanoTime() - t1;
+        Duration d = Duration.ofNanos( nanoDuration );
+        System.out.println( d );
+    }
+
+    public static void newCounter() throws IOException {
+        long t1 = System.nanoTime();
+        String file = FileLoader.loadFile();
+        String[] words = StringSplitter.getWords( file );
+        Stream<String> wordsStream = Arrays.stream( words );
+
+        String strWords = wordsStream.reduce( " ", (acc, n) -> n + "\n" + acc);
+        System.out.println( strWords );
+        long nanoDuration = System.nanoTime() - t1;
+        Duration d = Duration.ofNanos( nanoDuration );
+        System.out.println( d );
     }
 
     static class ReverseIterator implements Iterator<String> {
@@ -47,4 +77,5 @@ public class Counter5ReverseIterator {
             return null;
         }
     }
+
 }
