5fe10c5e0f7a2344f6294db0d005d4163ab84ce2
foreg <dmitry.strokov@mail.ru>
2018-07-10 18:28:12 +0500

    дз по stream api и lambda

--- src/main/java/ru/sbt/collections/Counter3DifferentWordsCount.java
+++ src/main/java/ru/sbt/collections/Counter3DifferentWordsCount.java
@@ -5,8 +5,11 @@ import ru.sbt.collections.utils.StringSplitter;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Подсчитайте и выведите на экран сколько раз каждое слово встречается в файле.
@@ -14,8 +17,13 @@ import java.util.Map;
 public class Counter3DifferentWordsCount {
 
     public static void main( String[] args ) throws IOException, URISyntaxException {
+        oldCounter3DifferentWordsCount(); //0.06
+        newCounter3DifferentWordsCount(); //0.26
+    }
+    static void oldCounter3DifferentWordsCount () throws IOException {
+        long t1 = System.nanoTime();
         String file = FileLoader.loadFile();
-        String[] words = StringSplitter.getWords( file );
+        String[] words = StringSplitter.getWordsOld( file );
         Map<String, Integer> map = new HashMap<>();
         for ( String word : words ) {
             int value = 1;
@@ -25,7 +33,22 @@ public class Counter3DifferentWordsCount {
             map.put( word, Integer.valueOf( value ) );
         }
 
+
         System.out.println( map );
+        long nanoDuration = System.nanoTime() - t1;
+        Duration d = Duration.ofNanos( nanoDuration );
+        System.out.println( d );
         System.out.println( "" + map.size() + '/' + words.length );
+    }
+    static void newCounter3DifferentWordsCount () throws IOException {
+        long t1 = System.nanoTime();
+        String file = FileLoader.loadFile();
+        Stream<String> stream = StringSplitter.getWords( file );
+        stream.collect(Collectors.groupingBy((p) -> p, Collectors.counting())).forEach((key,value)-> System.out.println(key + " " + value));
+        long nanoDuration = System.nanoTime() - t1;
+        Duration d = Duration.ofNanos( nanoDuration );
+        System.out.println( d );
+        //System.out.println( map );
+        //System.out.println( "" + map.size() + '/' + words.length );
     }
 }
--- src/main/java/ru/sbt/collections/utils/StringSplitter.java
+++ src/main/java/ru/sbt/collections/utils/StringSplitter.java
@@ -1,15 +1,26 @@
 package ru.sbt.collections.utils;
 
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
 /** Util class for splitting string
  * Created by Home on 02.07.2018.
  */
 public class StringSplitter {
-    public static String[] getWords( String file ) {
+    public static String[] getWordsOld(String file ) {
         String regex = "[;" + System.lineSeparator() + " .,]";
         return file.split( regex, -1 );
     }
 
-    public static String[] getLines( String file ) {
+    public static String[] getLinesOld(String file ) {
         return file.split( System.lineSeparator() );
+    }
+    public static Stream<String> getWords(String file ) {
+        String regex = "[;" + System.lineSeparator() + " .,]";
+        return Pattern.compile(regex).splitAsStream(file);
+    }
+
+    public static Stream<String> getLines( String file ) {
+        return Pattern.compile(System.lineSeparator()).splitAsStream(file);
     }
 }
